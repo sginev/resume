49f55c3b5e3201a106f06a548ea933b569cf8a4a
Stefan <choephix@gmail.com>
2023-01-18 18:36:19 +0000

    Fix footer

--- components/TheFooter.tsx
+++ components/TheFooter.tsx
@@ -6,17 +6,24 @@ export function TheFooter() {
     <footer className='footer'>
       <div className='text-center'>
         <small className='copyright'>
+          This website is built with
+          <a href='https://nextjs.org/' target='_blank' rel='noreferrer'>
+            {' Next.js 13 '}
+          </a>
+          and
+          <a href='https://tina.io' target='_blank' rel='noreferrer'>
+            {' TinaCMS '}
+          </a>
+        </small>
+      </div>
+
+      <div className='text-center'>
+        <small className='copyright'>
           Template designed with <FontAwesomeIcon icon={faHeart} /> by
           <a href='http://themes.3rdwavemedia.com' target='_blank' rel='noreferrer'>
             {' Xiaoying Riley '}
           </a>
           for developers
-        </small>
-      </div>
-      <div className='text-center'>
-        <small className='copyright'>
-          This website is built with <a href='https://nextjs.org/'>{' Next.js 13 '}</a> and
-          <a href='https://tina.io'>{' TinaCMS '}</a>
         </small>
       </div>
     </footer>
